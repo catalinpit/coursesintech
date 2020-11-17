6b543bbc566a394758bbe6c06e5e259116b3bf61
Catalin Pit <catalinpit@gmail.com>
2020-11-17 10:44:58 +0200

    Added cors, helmet, morgan and eslint

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 //const bodyParser = require('body-parser');
+
 require('./src/db/mongoose');
 const courseRouter = require('./src/routers/course');
 const userRouter = require('./src/routers/user');
+const notFound = require('./src/middleware/notFound');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,17 +17,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 app.use(userRouter);
 app.use(courseRouter);
-
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-app.get('*', (req, res) => {
-    res.status(404).render('404');
-});
+app.use(notFound);
 
 app.listen(port, () => {
     console.log(`The application started on port ${port}`);
---
+++ src/middleware/notFound.js
@@ -0,0 +1,5 @@
+const notFound = (req, res) => {
+    res.status(404).render('404');
+};
+
+module.exports = notFound;
