4f99552708bf012cf447daed2fad3af614e7eb36
Catalin Pit <catalinpit@gmail.com>
2020-11-18 10:33:16 +0200

    Removed tailwindcss, postcss, views

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ const notFound = require('./src/middleware/notFound');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
 app.use(morgan('common'));
